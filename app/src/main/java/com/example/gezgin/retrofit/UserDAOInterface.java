bb9f29688b2021f0f563e1a2eeab94741b6e359e
Berke Dursunoglu <74206002+berkedursunoglu@users.noreply.github.com>
2022-06-18 17:52:47 +0300

    Update UserDAOInterface.java

--- app/src/main/java/com/example/gezgin/retrofit/UserDAOInterface.java
+++ app/src/main/java/com/example/gezgin/retrofit/UserDAOInterface.java
@@ -14,46 +14,46 @@ import retrofit2.http.POST;
 public interface UserDAOInterface {
 
 
-    @POST("gezginapp/insert_places.php")
+    @POST("x/insert_places.php")
     @FormUrlEncoded
     Call<CRUDResponse> set_places(@Field("places_name") String places_name, @Field("places_img") String places_img, @Field("places_rate") String places_rate,@Field("user_id") Integer user_id);
 
-    @POST("gezginapp/getfav_places.php")
+    @POST("x/getfav_places.php")
     @FormUrlEncoded
     Call<PlacesFav> getfav_places(@Field("user_id") int user_id);
 
-    @POST("gezginapp/deletefav_places.php")
+    @POST("x/deletefav_places.php")
     @FormUrlEncoded
     Call<CRUDResponse> delete_fav(@Field("places_id") int places_id);
 
-    @GET("gezginapp/all_users.php")
+    @GET("x/all_users.php")
     Call<UsersResponse> all_user();
 
-    @POST("gezginapp/insert_users.php")
+    @POST("x/insert_users.php")
     @FormUrlEncoded
     Call<CRUDResponse> user_add(@Field("user_name") String user_name, @Field("user_password") String user_password, @Field("user_email") String user_email);
 
-    @POST("gezginapp/login_response.php")
+    @POST("x/login_response.php")
     @FormUrlEncoded
     Call<UsersResponse> login_response(@Field("username") String username);
 
-    @POST("gezginapp/username_check.php")
+    @POST("x/username_check.php")
     @FormUrlEncoded
     Call<CRUDResponse> username_check(@Field("username") String username);
 
-    @POST("gezginapp/email_check.php")
+    @POST("x/email_check.php")
     @FormUrlEncoded
     Call<CRUDResponse> email_check(@Field("useremail") String useremail);
 
-    @POST("gezginapp/delete_account.php")
+    @POST("x/delete_account.php")
     @FormUrlEncoded
     Call<CRUDResponse> delete_account(@Field("userid") int userid);
 
-    @POST("gezginapp/username_change.php")
+    @POST("x/username_change.php")
     @FormUrlEncoded
     Call<CRUDResponse> username_change(@Field("userid") int userid, @Field("username")String username);
 
-    @POST("gezginapp/password_change.php")
+    @POST("x/password_change.php")
     @FormUrlEncoded
     Call<CRUDResponse> password_change(@Field("userid") int userid, @Field("userpassword")String userpassword);
 
